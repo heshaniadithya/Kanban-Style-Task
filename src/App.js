015f6f8e4ae77b06b0ca6f83a6fef7119ddb1bc4
heshaniadithya <heshaniadithya2006@gmail.com>
2024-03-30 16:56:25 +0530

    kanban-style-task

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useDrop } from "react-dnd";
 import AddTask from "./components/AddTask";
+import ToDo from "./components/ToDo";
 
 function App() {
   const [taskList, setTaskList] = useState([]);
+  const [Completed, setCompleted] = useState([])
+  
+  useEffect(() => {
+    let array = localStorage.getItem("taskList");
+
+    if (array) {
+      setTaskList(JSON.parse(array))
+    }
+  }, [])
+
+  const [{ isOver }, drop] = useDrop(() => ({
+      accept: "todo",
+      drop: (item) => addToCompleted(item.id, item.prjectName, item.taskDescription, item.timestamp, item.duration),
+      collect: (monitor) => ({
+        isOver: !!monitor.isOver(),
+      })
+  }))
+
+  const addToCompleted = (id, prjectName, taskDescription, timestamp, duration) => {
+    const moveTask = taskList.filter((task) => id === task.id);
+    setCompleted((completed) => [...completed, {moveTask, prjectName, taskDescription, timestamp, duration}])
+  }
 
   return (
     <>
@@ -10,11 +34,28 @@ function App() {
       <p className="text-xl pl-6">Hi there!</p>
       <div className="flex flex-row items-center">
        <p className="text-xl pl-6">Click</p>
-       <AddTask taskList={taskList} setTaskList={setTaskList}/>
+       <AddTask taskList={taskList} setTaskList={setTaskList} />
       <p className="text-xl my-2">to add a new task</p> 
+      </div>
+      <div className="flex flex-row">
+      <div className="w-full">
+        <h2 className="ml-6 text-xl font-semibold w-3/4 max-w-lg my-4 py-2 px-4 bg-gray-200">To Do:</h2>
+      <div className="ml-6 flex flex-col-reverse">
+      {taskList.map((task, i) => 
+      <ToDo key={i} task={task} index={i} taskList={taskList} setTaskList={setTaskList} />
+      )}
+      </div>
+      </div>
+      <div className="w-full flex flex-col" ref={drop}>
+      <h2 className="ml-6 text-xl font-semibold w-3/4 max-w-lg my-4 py-2 px-4 bg-gray-200">Completed:</h2>
+      {Completed.map((task, i) => 
+      <ToDo key={i} task={task} taskList={taskList} setTaskList={setTaskList} />
+      )}
+      </div>
       </div>
     </>
   );
+
 }
 
 export default App;  
--- src/components/EditTask.jsx
+++ src/components/EditTask.jsx
@@ -1,63 +1,54 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-const AddTask = ({taskList, setTaskList}) => {
-    const [addModal, setAddModal] = useState(false);
+const EditTask = ({ task, taskList, setTaskList }) => {
+    const [editModal, setEditModal] = useState(false);
     const [projectName, setProjectName] = useState("");
     const [taskDescription, setTaskDescription] = useState("");
-    const [errorMessage, setErrorMessage] = useState("");
- 
+
+    useEffect(() => {
+       setProjectName(task.projectName);
+       setTaskDescription(task.taskDescription);
+    }, [])
+
     const handleInput = e => {
         const {name, value} = e.target;
 
-        if (name === "projectName") { 
-            setProjectName(value) 
-            setErrorMessage("");
-        }
-        if (name === "projectName" && value === "") {
-            setErrorMessage("Enter project name to continue");
-        }
+        if (name === "projectName") setProjectName(value)
         if (name === "taskDescription") setTaskDescription(value)
     }
 
-    const handleAdd = e => {
+    const handleUpdate = e => {
         e.preventDefault();
-        if (!projectName) {
-            setErrorMessage("Enter project name to continue")
-        } else {
-            let timestamp = new Date();
-            let tempList = taskList;
-            tempList.push({
-                projectName,
-                taskDescription, 
-                timestamp: timestamp,
-                duration: 0
-            })
-            localStorage.setItem("taskList", JSON.stringify(tempList))
-            window.location.reload()
-            setAddModal(false);
-            setProjectName("");
-            setTaskDescription("");
-        }
+        let taskIndex = taskList.indexOf(task);
+        taskList.splice(taskIndex, 1, {
+           projectName: projectName,
+           taskDescription: taskDescription,
+           timestamp: task.timestamp, 
+           duration: task.duration
+        });
+    localStorage.setItem("taskList", JSON.stringify(taskList));
+    window.location.reload();
+    setEditModal(false);    
+      
     }
 
-    return (
+    return ( 
         <>
-            <button
-               className="bg-blue-500 text-white uppercase text-sm font-semibold py-1 mx-1.5 pl-2 pr-2.5 rounded hover:opacity-70"
-               type="button"
-               onClick={() => setAddModal(true)}
-            >
-               +New
+        <button 
+        className="bg-gray-400 text-white text-sm-uppercase font-semibold py-1.5 px-3 rounded-lg"
+        onClick={()=>setEditModal(true)}
+        >
+            Edit
             </button>
-            {addModal ? (
-                <>
-                    <div className="flex items-center justify-center overflow-x-hidden overflow-y-auto fixed inset-0 z-100">
+        {editModal ? (
+        <>
+        <div className="flex items-center justify-center overflow-x-hidden overflow-y-auto fixed inset-0 z-100">
                         <div className="w-9/12 max-w-lg bg-white rounded-lg shadow-md relative flex flex-col">
                             <div className="flex flex-row justify-between p-5 border-b border-slate-200 rounded-t">
-                                <h3 className="text-3xl font-semibold">Add New Task</h3>
+                            <h3 className="text-3xl font-semibold">Edit Task</h3>
                         <button
                         className="px-1 text-gray-400 float-right text-3xl leading-none font-semibold block" 
-                        onClick={() => setAddModal(false)}  
+                        onClick={() => setEditModal(false)}  
                         >
                             x
                         </button>
@@ -68,7 +59,7 @@ const AddTask = ({taskList, setTaskList}) => {
                                         Project Name
                                     </label>
                                     <input
-                                        className="w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white"
+                                        className="w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-5 leading-tight focus:outline-none focus:bg-white"
                                         id="project-name"
                                         name="projectName"
                                         type="text"
@@ -76,8 +67,7 @@ const AddTask = ({taskList, setTaskList}) => {
                                         value={projectName}
                                         onChange={handleInput}
                                         required
-                                    />  
-                                    <p className="text-red-500 text-center mt-2 mb-5">{errorMessage}</p> 
+                                    />   
                                 </div>
                         <div>
                             <label
@@ -98,18 +88,17 @@ const AddTask = ({taskList, setTaskList}) => {
                             <div className="flex justify-end p-6 border-t border-slate-200 rounded-b">
                                 <button
                                 className="bg-blue-500 text-white font-semibold uppercase text-sm px-6 py-3 rounded hover:opacity-70"
-                                onClick={handleAdd}
+                                onClick={handleUpdate}
                                 >
-                                    Add Task
+                                    Update Task
                                 </button>
                             </div>
                         </div>   
                     </div>           
-                        
-                </>
-            ) : null}
-        </> 
-    )   
+        </>
+        ) : null}
+        </>
+     );
 }
-                           
-export default AddTask;
+ 
+export default EditTask;
